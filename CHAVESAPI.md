03fe78b9484fe42a543a92f8302cf0635950ba39
Valky23 <82124403+Valky23@users.noreply.github.com>
2021-04-08 15:33:19 -0500

    Add files via upload

---
+++ CHAVESAPI.md
@@ -0,0 +1,94 @@
+# Tutorial para obter as chaves das API's
+
+
+## 1 - RemoveBG (Remover fundo de fotos)
+
+#### Limite Gratuito: 50 stickers sem fundo/conta, após isso você terá que criar uma nova conta.
+
+
+### Crie uma conta no site https://accounts.kaleido.ai/users/sign_up?locale=pt-br
+
+Após criar a conta, clique em **API Key**
+<img src="https://i.imgur.com/llKh6JL.png" >
+
+Após apertar em **"Show"**, a sua **chave** será exibida e deverá ser usada no campo **API_REMOVEBG** no .env do Bot.
+<img src="https://i.imgur.com/kyANXdb.png"/>
+
+### Pronto, você já tem sua chave RemoveBG .
+
+
+
+
+<br>
+
+## 2 - NewsAPI (Noticias)
+
+### Crie uma conta no site https://newsapi.org/register
+
+Após a criação da conta a sua **chave** já será exibida para ser usada no campo **API_NEWS_ORG** no .env do Bot.
+<img src="https://i.imgur.com/WteuFSj.png">
+
+### Pronto, você já tem sua chave NewsAPI .
+
+<br>
+
+## 3 - RapidAPI (Pesquisa Imagens/Web)
+#### Limite Gratuito: 1000 pesquisas/conta, após isso você terá que criar uma nova conta.
+
+### Crie uma conta no site https://rapidapi.com/
+
+Após a conta ser criada, vá em **My Apps**
+<img src="https://i.imgur.com/WBrfNFU.png">
+
+Clique em **default-application_XXXXXX** e depois em **Security**
+<img src="https://i.imgur.com/UG0a6oI.png">
+
+E aqui vai estar a **chave** para usar no campo designado **API_RAPIDAPI** no .env do Bot.
+<img src="https://i.imgur.com/2iLagG6.png">
+
+#### Ainda não acabou, temos que nos inscrever nas API's do Bing para podermos usar os serviços deles.
+
+Se inscrevendo no **Bing Web Search** : https://rapidapi.com/microsoft-azure-org-microsoft-cognitive-services/api/bing-web-search1/pricing
+
+Clique em **"Select Plan"** para se inscrever no plano gratuito
+<img src="https://i.imgur.com/m7I0Mwo.png">
+
+Se inscrevendo no **Bing Image Search** : https://rapidapi.com/microsoft-azure-org-microsoft-cognitive-services/api/bing-image-search1/pricing
+
+Faça a mesma coisa da ultima inscrição, clique em **"Select Plan"**
+
+### Pronto, você já tem sua chave RapidAPI totalmente configurada.
+
+<br>
+
+## 4 - ACRCloud - Reconhecimento de Músicas
+
+#### Limite Gratuito: 14 dias de uso ou 5000 reconhecimentos, após isso o limite é de 100 reconhecimentos/dia.
+
+### Crie uma conta no site https://console.acrcloud.com/avr?region=eu-west-1#/register
+
+**IMPORTANTE**: Se houver algum problema para chegar a mensagem de verificação de email, crie outra conta usando qualquer VPN (Geralmente quando é a sua primeira conta sempre chega a mensagem, mas a partir da segunda pode ser necessário o uso do VPN)
+
+Após a conta criada, email verificado e um formulário básico inicial preenchido.. Você chegará nesta tela, clique em **Audio & Video Recognition**
+<img src="https://i.imgur.com/CeKSsjO.png"/>
+
+Abra a aba de **Projects** e clique em **Audio & Video Recognition**
+<img src="https://i.imgur.com/owgNhyv.png"/>
+
+Agora clique em **Create Project** e depois preencha os dados como mostrado abaixo:
+<img src="https://i.imgur.com/530RtLA.png"/>
+
+Nessa tela agora você tem seu **acr_host**, **acr_access_key** e **acr_access_secret** respectivamente, copie eles e coloque-os no .env do Bot.
+
+<img src="https://i.imgur.com/BA8aUA5.png"/>
+
+### Pronto, você já tem suas chaves ACRCloud .
+
+
+
+
+
+
+
+
+
---
+++ msgTratamento.js
@@ -0,0 +1,172 @@
+//REQUERINDO MODULOS
+const fs = require('fs-extra')
+const cadastrarGrupo = require('./lib/cadastrarGrupo')
+const db = require('./lib/database')
+const {botInfoUpdate, botLimitarComando, botInfo, botVerificarExpiracaoLimite,botLimitarMensagensPv} = require("./lib/bot")
+const {verificarBloqueioGlobal, verificarBloqueioGrupo} = require("./lib/bloqueioComandos")
+const { criarTexto, guiaComandoMsg, removerNegritoComando, consoleErro, consoleComando } = require('./lib/util')
+const msgs_texto = require('./lib/msgs')
+
+//COMANDOS
+const lista_comandos = JSON.parse(fs.readFileSync('./comandos/comandos.json'))
+const grupo = require('./comandos/grupo'), utilidades = require('./comandos/utilidades'), diversao = require('./comandos/diversao'), admin = require('./comandos/admin'), info = require('./comandos/info'), figurinhas = require('./comandos/figurinhas'), downloads = require('./comandos/downloads')
+
+
+module.exports = msgTratamento = async (client, message) => {
+    try {
+        const {t, sender, isGroupMsg, chat, type, caption, id, from, body} = message
+        const {formattedTitle} = chat, { pushname, verifiedName, formattedName } = sender, username = pushname || verifiedName || formattedName
+        const commands = caption || body || ''
+        var command = commands.toLowerCase().split(' ')[0] || ''
+        command = removerNegritoComando(command)
+        const args =  commands.split(' ')
+        const ownerNumber = process.env.NUMERO_DONO.trim()
+        const isOwner = ownerNumber == sender.id.replace(/@c.us/g, '')
+        const groupId = isGroupMsg ? chat.groupMetadata.id : ''
+        const groupAdmins = isGroupMsg ? await client.getGroupAdmins(groupId) : ''
+        const isGroupAdmins = isGroupMsg ? groupAdmins.includes(sender.id) : false
+        const grupoInfo = isGroupMsg ? await db.obterGrupo(groupId) : ''
+        const msgGuia = (args.length == 1) ? false : args[1] == "guia"
+        const blockedNumbers = await client.getBlockedIds()
+        const isBlocked = blockedNumbers.includes(sender.id)
+        const comandoExiste = (
+            lista_comandos.utilidades.includes(command) ||
+            lista_comandos.grupo.includes(command) || 
+            lista_comandos.diversao.includes(command) ||
+            lista_comandos.admin.includes(command) ||
+            lista_comandos.info.includes(command) ||
+            lista_comandos.figurinhas.includes(command) ||
+            lista_comandos.downloads.includes(command)
+        )
+       
+        //SE O GRUPO NÃO FOR CADASTRADO
+        if(isGroupMsg && !grupoInfo) await cadastrarGrupo(message,"msg",client)
+
+        //SE NÃO FOR MENSAGEM DE GRUPO E FOR  BLOQUEADO RETORNE
+        if (!isGroupMsg && isBlocked) return
+
+        //SE O CONTADOR TIVER ATIVADO E FOR UMA MENSAGEM DE GRUPO, VERIFICA SE O USUARIO EXISTE NO CONTADOR , REGISTRA ELE E ADICIONA A CONTAGEM
+        if(isGroupMsg && grupoInfo.contador.status) {
+            await db.existeUsuarioContador(groupId,sender.id)
+            await db.addContagem(groupId,sender.id,type)
+        }
+
+        //SE FOR ALGUM COMANDO EXISTENTE
+        if(comandoExiste){
+            let registrado = await db.verificarRegistro(sender.id)
+            
+            //4.0.1 - SE O USUARIO NÃO FOR REGISTRADO, FAÇA O REGISTRO
+            if(!registrado) {
+                if(isOwner) {
+                    await db.verificarDonoAtual(sender.id)
+                    await db.registrarDono(sender.id, username)
+                }
+                else {
+                    await db.registrarUsuarioComum(sender.id, username)
+                }
+            } else {
+                if(isOwner) await db.verificarDonoAtual(sender.id)       
+            }
+
+            //ATUALIZE NOME DO USUÁRIO 
+            await db.atualizarNome(sender.id, username)
+
+            //SE FOR MENSAGEM DE GRUPO E USUARIO FOR BLOQUEADO RETORNE
+            if (isGroupMsg && isBlocked) return
+
+            //SE O GRUPO ESTIVER COM O RECURSO 'MUTADO' LIGADO E USUARIO NÃO FOR ADMINISTRADOR
+            if(isGroupMsg && !isGroupAdmins && grupoInfo.mutar) return
+
+            //LIMITACAO DE COMANDO POR MINUTO
+            if(botInfo().limitecomandos.status){
+                let usuario = await db.obterUsuario(sender.id)
+                let limiteComando = await botLimitarComando(sender.id, usuario.tipo,isGroupAdmins)
+                if(limiteComando.comando_bloqueado) {
+                    if(limiteComando.msg != undefined) await client.reply(from, limiteComando.msg, id)
+                    return 
+                }
+            }
+            
+            //BLOQUEIO GLOBAL DE COMANDOS
+            if(await verificarBloqueioGlobal(command) && !isOwner){
+                return client.reply(from, criarTexto(msgs_texto.admin.bcmdglobal.resposta_cmd_bloqueado, command), id)
+            }
+            
+            //SE FOR MENSAGEM DE GRUPO , COMANDO ESTIVER BLOQUEADO E O USUARIO NAO FOR ADMINISTRADOR DO GRUPO
+            if(isGroupMsg && await verificarBloqueioGrupo(command, groupId) && !isGroupAdmins) return client.reply(from,criarTexto(msgs_texto.grupo.bcmd.resposta_cmd_bloqueado, command), id)
+
+            //SE O RECURSO DE LIMITADOR DIARIO DE COMANDOS ESTIVER ATIVADO E O COMANDO NÃO ESTIVER NA LISTA DE EXCEÇÔES/INFO/GRUPO/ADMIN
+            if(botInfo().limite_diario.status){
+                if(!lista_comandos.excecoes_contagem.includes(command) && !lista_comandos.admin.includes(command) && !lista_comandos.grupo.includes(command) && !lista_comandos.info.includes(command) && !msgGuia){
+                    await botVerificarExpiracaoLimite()
+                    let ultrapassou = await db.ultrapassouLimite(sender.id)
+                    if(!ultrapassou) await db.addContagemDiaria(sender.id) 
+                    else return client.reply(from, criarTexto(msgs_texto.admin.limitediario.resposta_excedeu_limite, username, ownerNumber), id)
+                } else {
+                    await db.addContagemTotal(sender.id)
+                    await botVerificarExpiracaoLimite()
+                }
+            } else {
+                await db.addContagemTotal(sender.id)
+            }
+          
+            //ADICIONA A CONTAGEM DE COMANDOS EXECUTADOS PELO BOT
+            await botInfoUpdate()
+
+        } else { //SE NÃO FOR UM COMANDO EXISTENTE
+            //SE FOR UMA MENSAGEM PRIVADA E O LIMITADOR DE MENSAGENS ESTIVER ATIVO
+            if(!isGroupMsg && botInfo().limitarmensagens.status){
+                let u = await db.obterUsuario(sender.id)
+                let tipo_usuario_pv = u ? u.tipo : "comum"
+                let limitarMensagens = await botLimitarMensagensPv(sender.id, tipo_usuario_pv)
+                if(limitarMensagens.bloquear_usuario) {
+                    await client.sendText(sender.id, limitarMensagens.msg)
+                    return await client.contactBlock(sender.id)
+                }
+            }
+        }
+
+        //APÓS TODAS AS VERIFICAÇÕES SOLICITE OS COMANDOS
+        if(lista_comandos.utilidades.includes(command)){
+            //UTILIDADES
+            if(msgGuia) return client.reply(from, guiaComandoMsg("utilidade", command), id)
+            await utilidades(client,message)
+            consoleComando(isGroupMsg, "UTILIDADES", command, "#de9a07", t, username, formattedTitle)
+        } else if(lista_comandos.info.includes(command)){
+            //INFO
+            if(msgGuia) return client.reply(from, guiaComandoMsg("info", command), id)
+            await info(client,message)
+            consoleComando(isGroupMsg, "INFO", command, "#8ac46e", t, username, formattedTitle)
+        } else if(lista_comandos.figurinhas.includes(command)){
+            //FIGURINHAS
+            if(msgGuia) return client.reply(from, guiaComandoMsg("figurinhas", command), id)
+            await figurinhas(client,message)
+            consoleComando(isGroupMsg, "FIGURINHAS", command, "#ae45d1", t, username, formattedTitle)
+        } else if(lista_comandos.downloads.includes(command)){
+            //DOWNLOADS
+            if(msgGuia) return client.reply(from, guiaComandoMsg("downloads", command), id)
+            await downloads(client,message)
+            consoleComando(isGroupMsg, "DOWNLOADS", command, "#2195cf", t, username, formattedTitle)
+        } else if (lista_comandos.grupo.includes(command)){
+            //GRUPO
+            if(msgGuia) return client.reply(from, guiaComandoMsg("grupo", command), id)
+            await grupo(client,message)
+            if(isGroupMsg) consoleComando(isGroupMsg, "ADMINISTRAÇÃO", command, "#e0e031", t, username, formattedTitle)
+        } else if(lista_comandos.diversao.includes(command)){
+            //DIVERSÃO
+            if(msgGuia) return client.reply(from, guiaComandoMsg("diversao", command), id)
+            await diversao(client,message)
+            consoleComando(isGroupMsg, "DIVERSÃO", command, "#22e3dd", t, username, formattedTitle)
+        } else if(lista_comandos.admin.includes(command)){
+            //ADMIN
+            if(msgGuia) return client.reply(from, guiaComandoMsg("admin", command), id)
+            await admin(client,message)
+            consoleComando(isGroupMsg, "DONO", command, "#d1d1d1", t, username, formattedTitle)
+        } else {
+            if(!isGroupMsg) return client.reply(from, msgs_texto.geral.comando_invalido ,id)
+        }
+
+    } catch (err) {
+        consoleErro(err, 'MSG')
+    }
+}
